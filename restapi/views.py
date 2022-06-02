2d4fe82ca3c7425fd9653de6f0a41f1450f2f3ca
yash-ti <yash.tripathi@trilogy.com>
2022-06-02 13:35:40 +0000

    Created utils.py for utility functions

--- restapi/views.py
+++ restapi/views.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals
 from decimal import Decimal
 import pandas as pd
 import numpy as np
-import urllib.request
-from datetime import datetime
 
 from django.http import HttpResponse
 from django.contrib.auth.models import User
@@ -20,7 +18,7 @@ from restapi.models import *
 from restapi.serializers import *
 from restapi.custom_exception import *
 
-
+from utils import normalize, sort_by_time_stamp, response_format, transform, aggregate, multi_threaded_reader
 
 def index(_request):
     return HttpResponse("Hello, world. You're at Rest.")
@@ -52,27 +50,6 @@ def balance(request):
     return Response(response, status=status.HTTP_200_OK)
 
 
-def normalize(expense):
-    user_balances = expense.users.all()
-    dues = {}
-    for user_balance in user_balances:
-        dues[user_balance.user] = dues.get(user_balance.user, 0) + user_balance.amount_lent \
-                                  - user_balance.amount_owed
-    dues = [(k, v) for k, v in sorted(dues.items(), key=lambda item: item[1])]
-    start = 0
-    end = len(dues) - 1
-    balances = []
-    while start < end:
-        amount = min(abs(dues[start][1]), abs(dues[end][1]))
-        user_balance = {"from_user": dues[start][0].id, "to_user": dues[end][0].id, "amount": amount}
-        balances.append(user_balance)
-        dues[start] = (dues[start][0], dues[start][1] + amount)
-        dues[end] = (dues[end][0], dues[end][1] - amount)
-        if dues[start][1] == 0:
-            start += 1
-        else:
-            end -= 1
-    return balances
 
 
 class UserViewSet(ModelViewSet):
@@ -197,76 +174,3 @@ def log_processor(request):
     response = response_format(data)
     return Response({"response":response}, status=status.HTTP_200_OK)
 
-def sort_by_time_stamp(logs):
-    data = []
-    for log in logs:
-        data.append(log.split(" "))
-    # print(data)
-    data = sorted(data, key=lambda elem: elem[1])
-    return data
-
-def response_format(raw_data):
-    response = []
-    for timestamp, data in raw_data.items():
-        entry = {'timestamp': timestamp}
-        logs = []
-        data = {k: data[k] for k in sorted(data.keys())}
-        for exception, count in data.items():
-            logs.append({'exception': exception, 'count': count})
-        entry['logs'] = logs
-        response.append(entry)
-    return response
-
-def aggregate(cleaned_logs):
-    data = {}
-    for log in cleaned_logs:
-        [key, text] = log
-        value = data.get(key, {})
-        value[text] = value.get(text, 0)+1
-        data[key] = value
-    return data
-
-
-def transform(logs):
-    result = []
-    for log in logs:
-        [_, timestamp, text] = log
-        text = text.rstrip()
-        timestamp = datetime.utcfromtimestamp(int(int(timestamp)/1000))
-        hours, minutes = timestamp.hour, timestamp.minute
-        key = ''
-
-        if minutes >= 45:
-            if hours == 23:
-                key = "{:02d}:45-00:00".format(hours)
-            else:
-                key = "{:02d}:45-{:02d}:00".format(hours, hours+1)
-        elif minutes >= 30:
-            key = "{:02d}:30-{:02d}:45".format(hours, hours)
-        elif minutes >= 15:
-            key = "{:02d}:15-{:02d}:30".format(hours, hours)
-        else:
-            key = "{:02d}:00-{:02d}:15".format(hours, hours)
-
-        result.append([key, text])
-        print(key)
-
-    return result
-
-
-def reader(url, timeout):
-    with urllib.request.urlopen(url, timeout=timeout) as conn:
-        return conn.read()
-
-
-def multi_threaded_reader(urls, num_threads):
-    """
-        Read multiple files through HTTP
-    """
-    result = []
-    for url in urls:
-        data = reader(url, 60)
-        data = data.decode('utf-8')
-        result.extend(data.split("\n"))
-    result = sorted(result, key=lambda elem:elem[1])
-    return result
