059a600376f253773bbe0b19cb14289630bf13f9
yash-ti <yash.tripathi@trilogy.com>
2022-06-02 13:08:35 +0000

    Replaced magic values in models.py with constants

--- restapi/models.py
+++ restapi/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import constants
 
 from django.db import models
 
@@ -8,17 +9,20 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, null=False)
+    name = models.CharField(max_length=constants.CATEGORY_NAME_MAX_LENGTH, null=False)
 
 
 class Groups(models.Model):
-    name = models.CharField(max_length=100, null=False)
+    name = models.CharField(max_length=constants.GROUP_NAME_MAX_LENGTH, null=False)
     members = models.ManyToManyField(User, related_name='members', blank=True)
 
 
 class Expenses(models.Model):
-    description = models.CharField(max_length=200)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=constants.EXPENSES_DESCRIPTION_MAX_LENGTH)
+    total_amount = models.DecimalField(
+        max_digits=constants.EXPENSES_AMOUNT_MAX_DIGITS, 
+        decimal_places=constants.EXPENSES_AMOUNT_DECIMAL_PLACES
+    )
     group = models.ForeignKey(Groups, null=True, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
 
@@ -26,8 +30,14 @@ class Expenses(models.Model):
 class UserExpense(models.Model):
     expense = models.ForeignKey(Expenses, default=1, on_delete=models.CASCADE, related_name="users")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="expenses")
-    amount_owed = models.DecimalField(max_digits=10, decimal_places=2)
-    amount_lent = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_owed = models.DecimalField(
+        max_digits=constants.USER_EXPENSE_AMOUNT_OWED_MAX_DIGITS, 
+        decimal_places=constants.USER_EXPENSE_AMOUNT_OWED_DECIMAL_PLACES
+    )
+    amount_lent = models.DecimalField(
+        max_digits=constants.USER_EXPENSE_AMOUNT_LENT_MAX_DIGITS, 
+        decimal_places=constants.USER_EXPENSE_AMOUNT_LENT_DECIMAL_PLACES
+    )
 
     def __str__(self):
         return f"user: {self.user}, amount_owed: {self.amount_owed} amount_lent: {self.amount_lent}"
